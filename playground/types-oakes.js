41bfab360340096323a863313c589233ef6e37ca
Patrick Oakes <patrickoakes1972@gmail.com>
2019-12-24 07:25:33 -0800

    commit strings

---
+++ playground/types-oakes.js
@@ -0,0 +1,33 @@
+/* eslint-disable */
+//const name = 'Patrick';
+//const middle  = "Steven";
+//const last = `Oakes`;
+//
+//const sentence = 'shes\'s so cool';
+//const sentence2 = `shes's so "cool"`;
+////backtick
+//const song = `Ohhh
+//
+//ya
+//
+//I like 
+//pizza`;
+//
+///*const hello = 'hello my name is ' + name + ". Nice to meet you";
+//
+//let hello2 = 'hello my name is ';
+//hello2 = hello2 + name;
+//hello2 = hello2 + ' Nice to meet you';*/
+//
+////backtick
+//const hello = `Hello my name is ${name}. Nice to meet you. I am ${1 +100} years old`;
+//
+//const html = `
+//<div>
+//<h2>${name}</h2>
+//<p>${hello}<p/>
+//</div>
+//
+//`;
+//document.body.innerHTML = html;
+
