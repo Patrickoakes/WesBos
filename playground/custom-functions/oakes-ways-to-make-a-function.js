f5308c97e1d449fcf629d85032d202d6e94a5cc4
Patrick Oakes <patrickoakes1972@gmail.com>
2019-12-28 13:24:30 -0800

    COMMIT 15 FINISH

--- playground/custom-functions/oakes-ways-to-make-a-function.js
+++ playground/custom-functions/oakes-ways-to-make-a-function.js
@@ -1,18 +1,24 @@
+//REGULAR FUNCTION
+
 //function doctorize(firstName) {
 //return `Dr. ${firstName}`;
 //}
 
-//anonymous function below//
+//ANONYMOUS function below//
+
 //function(firstName) {
 //    return `Dr. ${firstName}`;
 //    }
+
+
 //BELOW DOESNT WORK as "DOCTORIZE " because it is a variable function hoisting puts a 
 // function at the top
-console.log(doctorize('Patrick'));
-console.log(doctorize2('Patrick'));
+// console.log(doctorize('Patrick'));
+// console.log(doctorize2('Patrick'));
 
 
-// Function Expression WITHIN VARIABLE
+//FUNCTION EXPRESSION WITHIN VARIABLE
+
 //  const doctorize = function(firstName) {
 //       return `Dr. ${firstName}`;
 //       };
@@ -21,10 +27,133 @@ console.log(doctorize2('Patrick'));
 //    function doctorize2(firstName) {
 //    return `Dr. ${firstName}`;
 //   }
-        
-    // start at 8.21 with function expression
+        ///FUNCTIONS GO STRAIGHT TO TOP OF JAVA,   VARIABLES DO NOT////
 
-    // Function Expression WITHIN VARIABLE
-const doctorize = function(firstName) {
-    return `Dr. ${firstName}`;
-    };
+//ARROW FUNCTIONS :  NEWER LAST FEW YEARS NO SCOPE//
+//ARROW FUNCTION ALWAYS ANONYMOUS MUST USE VARIABLE
+
+
+//REGULAR FUNCTION
+
+//function inchToCM(inches) {
+//    const cm = inches * 2.54;
+//    return cm;
+//}
+
+//ARROW FUNCTION 
+// ONE PARAMETER OR ARGUMENT// NO PARENTHESES NEEDED
+
+const inchToCM = inches =>  inches * 2.54;
+
+
+/*REGULAR FUNCTION 
+funtion add(a, b = 3) {
+    const total = a + b;
+    return total;
+}             */
+
+
+//ARROW CONVERSION 
+//const add = (a, b = 3) => a + b;
+
+
+
+//NEW EXAMPLE 
+//function makeABaby(first, last) {
+//const baby = {
+//    name: `${first} ${last}`,
+//    age: 0
+//}
+//return baby;
+//}
+
+
+//ARROW FUNCTION 
+//const makeABaby = (first, last) =>  ({name: `${first} ${last}`,age: 0});
+    
+
+//IIFE
+// Immediately Invoked Function Expression
+
+// Doesn't WOrk below
+// function() {
+//    console.log(`Running the Anon function`);
+//    return `You are cool`;
+// }
+ 
+// WORKS BELOW////        WOW  WITH  (); ending
+//(function() {
+//        console.log(`Running the Anon function`);
+//        return `You are cool`;
+//   })();
+
+
+// METHODS METHODS METHODS
+// A FUNCTION THAT LIVES INSIDE OF AN OBJECT 
+
+// console =  Object
+// log     =  function
+//  console.log =  METHOD
+
+
+const patrick = {
+    name: 'Patrick Oakes',
+
+    //METHOD
+    sayHi: function(){
+        console.log(`Hi Pat`);
+        return 'Hey Patrick';
+    },
+
+    //SHORT HAND METHOD
+yellHi(){
+    console.log(`HEY PATRICKKKKKK`);
+},
+
+//ARROW FUNCTION
+wisperHi:  () => {
+    console.log(`hi patrickkkk im a mouse`);
+}
+}
+
+// CALLBACK FUNCTIONS  
+// Click Callback
+//const button = document.querySelector(`.clickMe`);
+//
+//console.log(button);
+//
+//button.addEventListener(`click`, patrick.yellHi);
+     
+// CALLBACK FUNCTIONS  
+// Click Callback
+//    const button = document.querySelector('.clickMe');
+//
+//   function handleClick() {
+//      console.log('Great Clicking!!');
+//    }
+//
+//    button.addEventListener('click',handleClick);
+
+
+
+// CALLBACK FUNCTIONS  
+// Click Callback
+const button = document.querySelector('.clickMe');
+
+   function handleClick() {
+      console.log('Great Clicking!!');
+    }
+
+    button.addEventListener('click', function(){
+        console.log(`Nice Job!!!!`);
+});
+
+
+//times callback
+//setTimeout(patrick.yellHi, 1000);
+
+
+//times callback ANONYMOUS
+setTimeout(function(){ 
+    console.log(`DONE! Time to eat!`);
+}, 1000);
